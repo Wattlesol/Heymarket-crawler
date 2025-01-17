09029257f999e0c1aa7aa7e82d513507dc0a0ced
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-01-18 02:50:48 +0500

    updates

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ def api_process_list():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
